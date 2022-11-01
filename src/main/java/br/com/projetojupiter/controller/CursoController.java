4aa1dd790b6ac48d388f09596bca3df4585933ad
DanieleStein <danieleastein@gmail.com>
2022-11-01 16:12:54 -0300

    Criada a classe controller de Curso

---
+++ src/main/java/br/com/projetojupiter/controller/CursoController.java
@@ -0,0 +1,14 @@
+package br.com.projetojupiter.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/curso")
+@CrossOrigin(origins = "*", allowedHeaders="*")
+public class CursoController {
+	
+	
+
+}
