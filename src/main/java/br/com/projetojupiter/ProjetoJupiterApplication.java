f90aed26ae19b75bb895811a431736094c676594
DanieleStein <danieleastein@gmail.com>
2022-11-01 09:55:35 -0300

    Initial

---
+++ src/main/java/br/com/projetojupiter/ProjetoJupiterApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetojupiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJupiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJupiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetojupiter/ProjetoJupiterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetojupiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJupiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
