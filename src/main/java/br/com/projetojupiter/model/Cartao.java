f986711409c5f84d731cab9eedecaf5e6b160298
DanieleStein <danieleastein@gmail.com>
2022-11-09 15:56:04 -0300

    Feito o link entre cartao e usuario

--- src/main/java/br/com/projetojupiter/model/Cartao.java
+++ src/main/java/br/com/projetojupiter/model/Cartao.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_cartao")
@@ -37,6 +40,19 @@ public class Cartao {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataValidade;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("cartao")
+	private Usuario usuario;
+	
+	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	public Date getDataValidade() {
 		return dataValidade;
--- src/main/java/br/com/projetojupiter/model/Usuario.java
+++ src/main/java/br/com/projetojupiter/model/Usuario.java
@@ -1,17 +1,22 @@
 package br.com.projetojupiter.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -42,9 +47,21 @@ public class Usuario {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dataNascimento;
+	
+	@OneToMany(mappedBy= "usuario", cascade=CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Cartao> cartao;
+	
 
 	
-	
+	public List<Cartao> getCartao() {
+		return cartao;
+	}
+
+	public void setCartao(List<Cartao> cartao) {
+		this.cartao = cartao;
+	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
