03c9228eb091891bc1605f2bdc52518496cae286
DanieleStein <danieleastein@gmail.com>
2022-11-14 17:02:29 -0300

    Somente alteração de linhas em branco

--- src/main/java/br/com/projetojupiter/service/CursoService.java
+++ src/main/java/br/com/projetojupiter/service/CursoService.java
@@ -2,15 +2,9 @@ package br.com.projetojupiter.service;
 
 
 import br.com.projetojupiter.model.Curso;
-import br.com.projetojupiter.model.Pedido;
-import br.com.projetojupiter.model.Usuario;
 import br.com.projetojupiter.repository.CursoRepository;
-import br.com.projetojupiter.repository.PedidoRepository;
-import br.com.projetojupiter.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
