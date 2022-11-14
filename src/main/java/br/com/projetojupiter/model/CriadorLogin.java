393e1390a4ae3eef58df6bf06724bbaac801ac72
DanieleStein <danieleastein@gmail.com>
2022-11-14 17:03:16 -0300

    Criado o LoginCriador no package model

---
+++ src/main/java/br/com/projetojupiter/model/CriadorLogin.java
@@ -0,0 +1,33 @@
+package br.com.projetojupiter.model;
+
+public class CriadorLogin {
+	
+	private String email;
+    private String senha;
+    private String token;
+    
+    
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	
+	}
+	public String getToken() {
+		return token;
+	}
+	
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
